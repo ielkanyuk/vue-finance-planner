d6c87696a9f6ba96c734fbd49bbc2cc082f5f644
Igor Elkanyuk <I.Elkanyuk@modulbank.ru>
2020-07-07 15:15:32 +0500

    Edit categories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,18 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
-        <CategoryCreate @created="addNewCategory"/>
-        <CategoryEdit />
+      <Loader v-if="loading"/>
+      <div class="row" v-else>
+        <CategoryCreate
+          @created="addNewCategory"
+        />
+        <CategoryEdit
+          v-if="categories.length"
+          :categories="categories"
+          :key="categories.length + updateCount"
+          @updated="updateCategory"
+        />
+        <p v-else class="center">Категорий пока нет</p>
       </div>
     </section>
   </div>
@@ -20,13 +29,26 @@ export default {
   name: 'Categories',
   data: () => ({
     categories: [],
+    loading: true,
+    updateCount: 0,
   }),
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.loading = false;
+  },
   components: {
     CategoryCreate, CategoryEdit,
   },
   methods: {
     addNewCategory(category) {
       this.categories.push(category);
+    },
+    updateCategory(category) {
+      const idx = this.categories.findIndex((c) => c.id === category.id);
+      this.categories[idx].title = category.title;
+      this.categories[idx].limit = category.limit;
+      // eslint-disable-next-line
+      this.updateCount++;
     },
   },
 };
